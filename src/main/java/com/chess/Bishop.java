d20c1149b18ba167c0f8737a766665d4772aa37f
Ghislain Demaël <58844111+LeCarnotzetVeveysan@users.noreply.github.com>
2023-03-06 14:25:46 +0100

    Added movements for Bishops

--- src/main/java/com/chess/Bishop.java
+++ src/main/java/com/chess/Bishop.java
@@ -1,5 +1,10 @@
 package com.chess;
 
+import java.util.ArrayList;
+
+import static com.utils.ModelUtils.getCoordinatesFromIndices;
+import static com.utils.ModelUtils.getIndicesFromCoordinates;
+
 public class Bishop extends Piece {
     public Bishop(char inColor, String inCurrentCell) {
         super("bishop", inColor, inCurrentCell);
@@ -7,6 +12,65 @@ public class Bishop extends Piece {
 
     @Override
     public void calculateAccessibleCells(Board board) {
+
+        ArrayList<String> accessibleCells = new ArrayList<>();
+        int[] currentPos = getIndicesFromCoordinates(super.getCurrentCell());
+
+        int i,j;
+
+        for (i = currentPos[0] + 1, j = currentPos[1] + 1; i <= 7 && j <= 7; i++, j++) {
+            String nCellBottomRightMove = getCoordinatesFromIndices(i, j);
+            Cell nCellBottomRight = board.getSpecificCell(nCellBottomRightMove);
+            if (!nCellBottomRight.getOccupied()) {
+                accessibleCells.add(nCellBottomRightMove);
+            } else if (nCellBottomRight.getOccupied() && nCellBottomRight.getPiece().getColor() != super.getColor()) {
+                accessibleCells.add(nCellBottomRightMove);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        for (i = currentPos[0] + 1, j = currentPos[1] - 1; i <= 7 && j >= 0; i++, j--) {
+            String nCellBottomLeftMove = getCoordinatesFromIndices(i, j);
+            Cell nCellBottomLeft = board.getSpecificCell(nCellBottomLeftMove);
+            if (!nCellBottomLeft.getOccupied()) {
+                accessibleCells.add(nCellBottomLeftMove);
+            } else if (nCellBottomLeft.getOccupied() && nCellBottomLeft.getPiece().getColor() != super.getColor()) {
+                accessibleCells.add(nCellBottomLeftMove);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        for (i = currentPos[0] - 1, j = currentPos[1] + 1; i >= 0 && j <= 7; i--, j++) {
+            String nCellTopRightMove = getCoordinatesFromIndices(i, j);
+            Cell nCellTopRight = board.getSpecificCell(nCellTopRightMove);
+            if (!nCellTopRight.getOccupied()) {
+                accessibleCells.add(nCellTopRightMove);
+            } else if (nCellTopRight.getOccupied() && nCellTopRight.getPiece().getColor() != super.getColor()) {
+                accessibleCells.add(nCellTopRightMove);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        for (i = currentPos[0] - 1, j = currentPos[1] - 1; i >= 0 && j >= 0; i--, j--) {
+            String nCellTopLeftMove = getCoordinatesFromIndices(i, j);
+            Cell nCellTopLeft = board.getSpecificCell(nCellTopLeftMove);
+            if (!nCellTopLeft.getOccupied()) {
+                accessibleCells.add(nCellTopLeftMove);
+            } else if (nCellTopLeft.getOccupied() && nCellTopLeft.getPiece().getColor() != super.getColor()) {
+                accessibleCells.add(nCellTopLeftMove);
+                break;
+            } else {
+                break;
+            }
+        }
+
+        super.setAccessibleCells(accessibleCells);
 
     }
 }
